f20433bb898b6e6787fda009c456583ec0db10de
Suraj Gaire <eriag321@gmail.com>
2022-06-16 00:57:43 +0545

    Testimonial added

---
+++ src/components/Testimonials/Testimonials.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Testimonial from "./Testimonial";
+
+function Testimonials() {
+  return (
+    <div className="Testimonials__container">
+      <Testimonial />
+      <Testimonial />
+      <Testimonial />
+    </div>
+  );
+}
+
+export default Testimonials;
