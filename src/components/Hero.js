2d3c1ca79b04a51f221c4b530ff0b7949b9dfa8d
Suraj Gaire <eriag321@gmail.com>
2022-06-23 01:04:09 +0545

    removed unnecessary comment in hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,20 +5,6 @@ import hero from "../images/hero1.jpg";
 function Hero() {
   return (
     <div className="hero__container">
-      {/* <div className="custom-shape-divider-top-1655666485">
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-            className="shape-fill"
-          ></path>
-        </svg>
-      </div> */}
-
       <div className="container">
         <div className="about">
           <p>Welcome to balchhi</p>
@@ -42,34 +28,6 @@ function Hero() {
         <div className="image">
           <img src={hero} alt="balchhi" />
         </div>
-        {/* <div class="custom-shape-divider-top-1655708468">
-          <svg
-            data-name="Layer 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1200 120"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-              class="shape-fill"
-            ></path>
-          </svg>
-
-        <div class="custom-shape-divider-top-1655708468">
-          <svg
-            data-name="Layer 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1200 120"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-              class="shape-fill"
-            ></path>
-          </svg>
-
-        </div> */}
-        {/* </div> */}
       </div>
     </div>
   );
